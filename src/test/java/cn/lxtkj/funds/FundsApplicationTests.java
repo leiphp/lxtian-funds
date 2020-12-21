40af9e6cf2d4fd750a17327bed1022b055f1425b
leiwen <657106593@qq.com>
2020-12-22 01:20:13 +0800

    init project

---
+++ src/test/java/cn/lxtkj/funds/FundsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lxtkj.funds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
