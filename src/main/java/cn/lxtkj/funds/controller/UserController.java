3e100b739b5c4928f7f912067e5b69a6d6a341fd
leiwen <657106593@qq.com>
2020-12-25 00:51:12 +0800

    feat；全局异常处理

--- src/main/java/cn/lxtkj/funds/controller/UserController.java
+++ src/main/java/cn/lxtkj/funds/controller/UserController.java
@@ -4,6 +4,7 @@ package cn.lxtkj.funds.controller;
 import cn.lxtkj.funds.common.lang.Result;
 import cn.lxtkj.funds.entity.User;
 import cn.lxtkj.funds.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @RequiresAuthentication
     @GetMapping("/index")
     public Result index(){
         User user = userService.getById(1L);
