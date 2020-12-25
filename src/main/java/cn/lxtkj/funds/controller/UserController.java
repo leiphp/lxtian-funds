e27c93352af74a39b6308ac5126efc20893f3d0d
leiwen <leiwen@jmfen.cn>
2020-12-25 20:02:55 +0800

    feat:登录接口及跨域问题解决

--- src/main/java/cn/lxtkj/funds/controller/UserController.java
+++ src/main/java/cn/lxtkj/funds/controller/UserController.java
@@ -6,10 +6,8 @@ import cn.lxtkj.funds.entity.User;
 import cn.lxtkj.funds.service.UserService;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -29,6 +27,11 @@ public class UserController {
     @GetMapping("/index")
     public Result index(){
         User user = userService.getById(1L);
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user){
         return Result.succ(user);
     }
 
